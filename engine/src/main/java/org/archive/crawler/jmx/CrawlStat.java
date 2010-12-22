82dad09a42ab16c437fa3d4053ba48f89f717cca
Kenji Nagahashi <kenji@archive.org>
2010-12-21 16:27:52 -0800

    JMX: CrawlStat minimized dependencies on StatisticsTracker and CrawlStatSnapshot. lazy creation of CrawlStatSnapshot.

--- engine/src/main/java/org/archive/crawler/jmx/CrawlStat.java
+++ engine/src/main/java/org/archive/crawler/jmx/CrawlStat.java
@@ -1,49 +1,58 @@
 package org.archive.crawler.jmx;
 
 import org.archive.crawler.framework.CrawlController;
+import org.archive.crawler.framework.Frontier;
 import org.archive.crawler.reporting.CrawlStatSnapshot;
 import org.archive.crawler.reporting.StatisticsTracker;
 
 /**
- * CrawlStat is a JavaBean compliant wrapper around {@link CrawlStatSnapshot}, which exposes
- * getters for those stats and hides unwanted methods.
+ * CrawlStat wraps around {@link CrawlStatSnapshot} and exposes
+ * JavaBean compliant getters for those stats and also hides unwanted methods.
  * <p>TODO I'm dropping this class in favor of more direct {@link FrontierReport} and others, because
- * CrawlStat replies on {@link StatisticsTracker} and {@link CrawlStatSnapshot}, that implement H3's own
+ * CrawlStat relies on {@link StatisticsTracker} and {@link CrawlStatSnapshot}, that implement H3's own
  * stat tracking mechanism. As JMX is meant for use with external stat tracking mechanism, H3 JMX interface
  * should not rely on those classes. This class is kept as extension of {@link FrontierReport} to
- * support existing monitoring code using this interface.</p>
- * <p>TODO urisFetched, byteProcessed, elapsedMilliseconds, busyThreads, and
- * totalThreads are only found in this class and still useful. We need to find a new
- * home for these stats before entirly dropping this class.</p>
+ * support existing monitoring code expecting this interface to exist. Now properties in this class are
+ * either not useful for monitoring through JMX, or are useful and only available through {@link StatisticsTracker}.</p>
+ * <p>TODO byteProcessed and elapsedMilliseconds
+ * are only found in {@link StatisticsTracker} and still useful. We need to find a new
+ * home for these stats before entirely dropping this class, probably {@link Frontier}.</p>
  * @see CrawlJobMXBean
  * @see CrawlJobManager
  * @contributor Kenji Nagahashi
  */
 public class CrawlStat extends FrontierReport {
+    private CrawlController controller;
     private CrawlStatSnapshot snapshot;
-    private int totalThreads;
+//    private StatisticsTracker stats;
     public CrawlStat(CrawlController controller) {
         super(controller.getFrontier());
-        StatisticsTracker stats = controller.getStatisticsTracker();
-        this.snapshot = stats.getSnapshot();
-        this.totalThreads = stats.threadCount();
+        this.controller = controller;
+//        this.stats = controller.getStatisticsTracker();
+//        StatisticsTracker stats = controller.getStatisticsTracker();
+//        this.snapshot = stats.getSnapshot();
+//        this.totalThreads = stats.threadCount();
     }
     /**
-     * time this snapshot was taken.
-     * @return {@link System#currentTimeMillis()} value.
+     * lazily obtain {@link CrawlStatSnapshot} object
+     * @return
      */
-    public long getTimestamp() {
-        return snapshot.timestamp;
+    protected CrawlStatSnapshot getSnapshot() {
+        if (snapshot == null)
+            snapshot = controller.getStatisticsTracker().getSnapshot();
+        return snapshot;
     }
-    public long getUrisFetched() {
-        return snapshot.urisFetched;
-    }
+    // dropped because nobody sets CrawlStatSnapshot#urisFetched
+//    public long getUrisFetched() {
+//        return snapshot.urisFetched;
+//    }
     /**
      * amounts of data downloaded.
      * @return bytes
      */
     public long getBytesProcessed() {
-        return snapshot.bytesProcessed;
+//        return snapshot.bytesProcessed;
+        return controller.getStatisticsTracker().getCrawledBytes().getTotalBytes();
     }
     /**
      * sum of {@link #getQueuedUriCount()}, {@link #busyThreads()} and
@@ -51,14 +60,16 @@ public class CrawlStat extends FrontierReport {
      * @return
      */
     public long getTotalUriCount() {
-        return snapshot.totalCount();
+        //return getSnapshot().totalCount();
+        // XXX duplicating CrawlStatSnapshot#totalCount()
+        return getQueuedUriCount() + getBusyThreads() + getDownloadedUriCount();
     }
     /**
      * time spent on crawling.
      * @return milli seconds
      */
     public long getElapsedMilliseconds() {
-        return snapshot.elapsedMilliseconds;
+        return controller.getStatisticsTracker().getCrawlElapsedTime();
     }
     /**
      * this number is average over whole uptime, derived from
@@ -66,7 +77,7 @@ public class CrawlStat extends FrontierReport {
      * @return double pages per second
      */
     public double getDocsPerSecond() {
-        return snapshot.docsPerSecond;
+        return getSnapshot().docsPerSecond;
     }
     /**
      * this is average since the last sample.
@@ -74,14 +85,14 @@ public class CrawlStat extends FrontierReport {
      * @return double pages per second
      */
     public double getCurrentDocsPerSecond() {
-        return snapshot.currentDocsPerSecond;
+        return getSnapshot().currentDocsPerSecond;
     }
     /**
      * derived from bytesProcessed and crawlElapsedTime.
      * @return double kilo bytes per second
      */
     public double getTotalKiBPerSec() {
-        return snapshot.totalKiBPerSec;
+        return getSnapshot().totalKiBPerSec;
     }
     /**
      * this is an average since the last sample.
@@ -89,16 +100,17 @@ public class CrawlStat extends FrontierReport {
      * @return double kilo bytes per second
      */
     public double getCurrentKiBPerSec() {
-        return snapshot.currentKiBPerSec;
+        return getSnapshot().currentKiBPerSec;
     }
     /**
      * number of active toe threads.
      * @return non-negative integer
      */
-    public int busyThreads() {
-        return snapshot.busyThreads;
+    public int getBusyThreads() {
+        return controller.getActiveToeCount();
     }
-    public int totalThreads() {
-        return totalThreads;
+    public int getTotalThreads() {
+//        return controller.getStatisticsTracker().threadCount();
+        return controller.getToeCount();
     }
 }
